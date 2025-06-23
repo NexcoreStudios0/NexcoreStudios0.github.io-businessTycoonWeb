90e376614de4bb8a6a31b829ad30fe02f23792ee
NexcoreStudios0 <145194679+NexcoreStudios0@users.noreply.github.com>
2025-06-23 11:03:49 +0300

    Telegram update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5991,14 +5991,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "2085b4ba9e2e086ece1c05fe10f7922e
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "2c9492e62e29ec327b6787bff689f147",
+"flutter_bootstrap.js": "6001768c4396a80069453961237a0b05",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "8b56c7dd3f375cf3d406512e8b2525a3",
-"/": "8b56c7dd3f375cf3d406512e8b2525a3",
-"main.dart.js": "4547dfe01fa79731d42f9d9248cacceb",
+"index.html": "2900799b8510c3f570c426cdb218d1e2",
+"/": "2900799b8510c3f570c426cdb218d1e2",
+"main.dart.js": "a6a8168f01f50ab28f828ec2ae3ff550",
 "manifest.json": "6da08a9e3e0171df26a49a98497d73b4",
 "version.json": "4cbbd020d22bb31e0e44dde02ffc8bf9"};
 // The application shell files that are downloaded before a service worker can
