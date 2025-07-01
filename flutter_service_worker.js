c97b87d568d718ed61c1700b9451cb4ce9fb87b3
NexcoreStudios0 <145194679+NexcoreStudios0@users.noreply.github.com>
2025-07-01 17:15:30 +0300

    update Icons

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6032,14 +6032,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "27a267e66d5a8b5ee31d2d32cab15cd2
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "67950c5fe914bdfbe10d8ba538444744",
+"flutter_bootstrap.js": "b3e02bdf8868befbef7f342cb85ae847",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "9b141c2084a9e4143d6a7dbdffce273c",
-"/": "9b141c2084a9e4143d6a7dbdffce273c",
-"main.dart.js": "05dda997bbbd93f1975af960ef07eb3f",
+"index.html": "3dc5fc2eaf541d38c4d87d1b24313150",
+"/": "3dc5fc2eaf541d38c4d87d1b24313150",
+"main.dart.js": "c649d0ca6a638d34aca61dab9a6e90cb",
 "manifest.json": "6da08a9e3e0171df26a49a98497d73b4",
 "version.json": "4cbbd020d22bb31e0e44dde02ffc8bf9"};
 // The application shell files that are downloaded before a service worker can
